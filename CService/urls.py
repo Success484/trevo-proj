3bfbc420f0eb26837866afe8e490c6c00c3f2c59
Success <successsimeon484@gmail.com>
2026-02-18 11:00:09 +0100

    feat: updating

--- CService/urls.py
+++ CService/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = []
+urlpatterns = [
+]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
